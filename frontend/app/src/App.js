540b206f25e9e2789c4d7387419affe49d95307a
Paulo Vinicius Cristaldo A. <102568081+Paulo-vca@users.noreply.github.com>
2023-09-13 05:32:19 +0000

    login

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -4,7 +4,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div className="login">
+          <h2>Login</h2>
+          <form className='login-form'>
+            <input type="email" name="email" placeholder="E-mail" requered/>
+            <input type="password" name="password" placehoder="Senha" requered/>
+            
+          </form>
+        </div>
       </header>
     </div>
   );
